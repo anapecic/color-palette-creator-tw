d281c3794dc0af5610aacd861b25904d48d1844e
Ana Pecic <ana.pecic97@gmail.com>
2024-08-27 22:26:16 +0200

    final

--- pages/index.js
+++ pages/index.js
@@ -142,7 +142,7 @@ export default function Home() {
   return (
     <div className="p-8 max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold text-center mb-8">
-        ✨ Theme Creator!! ✨
+        ✨ Palette Creator ✨
       </h1>
 
       <div className="mb-8">
